95a27d57f13904d7129bf8cff0b01d6c1881cf21
Raymond Edwards <raymond.edwards+github@gmail.com>
2015-04-18 20:44:33 -0300

    Rough completion of inline parser. Completely untested.

--- lib/rubycoma/inlineparser.rb
+++ lib/rubycoma/inlineparser.rb
@@ -63,6 +63,8 @@ module RubyCoMa
     REGEX_EMAILAUTOLINK         = /^<([a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*)>/
     REGEX_AUTOLINK              = /^<(?:coap|doi|javascript|aaa|aaas|about|acap|cap|cid|crid|data|dav|dict|dns|file|ftp|geo|go|gopher|h323|http|https|iax|icap|im|imap|info|ipp|iris|iris.beep|iris.xpc|iris.xpcs|iris.lwz|ldap|mailto|mid|msrp|msrps|mtqp|mupdate|news|nfs|ni|nih|nntp|opaquelocktoken|pop|pres|rtsp|service|session|shttp|sieve|sip|sips|sms|snmp|soap.beep|soap.beeps|tag|tel|telnet|tftp|thismessage|tn3270|tip|tv|urn|vemmi|ws|wss|xcon|xcon-userid|xmlrpc.beep|xmlrpc.beeps|xmpp|z39.50r|z39.50s|adiumxtra|afp|afs|aim|apt|attachment|aw|beshare|bitcoin|bolo|callto|chrome|chrome-extension|com-eventbrite-attendee|content|cvs|dlna-playsingle|dlna-playcontainer|dtn|dvb|ed2k|facetime|feed|finger|fish|gg|git|gizmoproject|gtalk|hcp|icon|ipn|irc|irc6|ircs|itms|jar|jms|keyparc|lastfm|ldaps|magnet|maps|market|message|mms|ms-help|msnim|mumble|mvn|notes|oid|palm|paparazzi|platform|proxy|psyc|query|res|resource|rmi|rsync|rtmp|secondlife|sftp|sgn|skype|smb|soldat|spotify|ssh|steam|svn|teamspeak|things|udp|unreal|ut2004|ventrilo|view-source|webcal|wtai|wyciwyg|xfire|xri|ymsgr):[^<>\x00-\x20]*>/i
     REGEX_HTMLTAG               = Regexp.new('^' << STRINGREGEX_HTMLTAG)
+    REGEX_FINALSPACE            = / *$/
+    REGEX_MAIN                  = /^[^\n`\[\]\\!<&*_'"]+/m
 
     def initialize
       @char_index = 0
@@ -118,12 +120,15 @@ module RubyCoMa
                          when CHARCODE_BACKSLASH
                            parse_backslash
                          when CHARCODE_BACKTICK
-                         when CHARCODE_COLON
+                           parse_backticks
                          when CHARCODE_EXCLAM
+                           parse_exclam
                          when CHARCODE_LEFTBRACKET
+                           parse_left_bracket
                          when CHARCODE_RIGHTBRACKET
                            parse_right_bracket
                          when CHARCODE_LESSTHAN
+                           parse_less_than
                          else
                            parse_string
                        end
@@ -137,6 +142,53 @@ module RubyCoMa
     end
 
     def handle_newline
+      lastc = @node.inlines
+      if lastc && lastc.style == :text
+        sps = REGEX_FINALSPACE.match(lastc.content)[0].length
+        add_inline(sps >= 2 ? :hardbreak : :softbreak)
+      else
+        add_inline(:softbreak)
+      end
+    end
+
+    def parse_left_bracket
+      startpos = @char_index
+      @char_index += 1
+
+      node = Inline.new(:text, '[')
+      @node.add_inline(node)
+
+      add_delimiter({:cc => CHARCODE_LEFTBRACKET,
+                     :numdelims => 1,
+                     :node => node,
+                     :can_open => true,
+                     :can_close => false,
+                     :index => startpos,
+                     :active => true
+                    })
+      true
+    end
+
+    def parse_exclam
+      startpos = @char_index
+      @char_index += 1
+      if peek == CHARCODE_LEFTBRACKET
+        @char_index += 1
+
+        node = Inline.new(:text, '![')
+        @node.add_inline(node)
+
+        add_delimiter({:cc => CHARCODE_EXCLAM,
+                       :numdelims => 1,
+                       :node => node,
+                       :can_open => true,
+                       :can_close => false,
+                       :index => startpos + 1,
+                       :active => true
+                      })
+      else
+        add_inline(:text, '!')
+      end
       true
     end
 
@@ -193,8 +245,11 @@ module RubyCoMa
       false
     end
 
-    def parse_html_tag
-      m = match(REGEX_HT)
+    def parse_less_than
+      m = match(REGEX_HTMLTAG)
+      return false if m.nil?
+      add_inline(:html, m)
+      true
     end
 
     def parse_link_destination
@@ -327,13 +382,16 @@ module RubyCoMa
                      :inline_node => inl,
                      :can_open => can_open,
                      :can_close => can_close,
-                     :active => true,
-                     :next => nil
+                     :active => true
                     })
       true
     end
 
     def parse_string
+      if m = match(REGEX_MAIN)
+        add_inline(:text, m)
+        return true
+      end
       false
     end
 
